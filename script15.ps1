bbfd0c1ddf5423488a889741d6934fc5208945dd
Thiago Smith Rocha Braz <106747435+thiagosmith@users.noreply.github.com>
2025-01-12 18:51:50 -0300

    Update script15.ps1

--- script15.ps1
+++ script15.ps1
@@ -1,5 +1,5 @@
 # Scripting Powershell
-$web = Invoke-WebRequest -uri "http://www.businesscorp.com.br" -Method Options
+$web = Invoke-WebRequest -uri "http://scanme.org" -Method Options
 echo "o servidor roda: "
 $web.headers.server
 echo ""
@@ -7,5 +7,5 @@ echo "o servidor aceita os metodos: "
 $web.headers.allow
 echo ""
 echo "Links encontrados: "
-$web2 = Invoke-WebRequest -uri "http://www.businesscorp.com.br"
+$web2 = Invoke-WebRequest -uri "http://scanme.org"
 $web2.links.href | Select-String http
