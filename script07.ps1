d039d4864035f5c34aeb69c1d5453d516d39b10a
Thiago Smith Rocha Braz <106747435+thiagosmith@users.noreply.github.com>
2025-01-12 18:44:55 -0300

    Update script07.ps1

--- script07.ps1
+++ script07.ps1
@@ -3,7 +3,7 @@ echo "Mentoria OffSec - Scripting PowerShell"
 echo "5m1th Security - OffSec Team"
 $age = Read-Host "qual a idade?"
 if ($age -ge "18"){
-    echo "pode dirigir"
+    echo "pode comprar bebida alcolica"
 }else{
-    echo "nao pode dirigir"
+    echo "nao pode comprar bebida alcolica"
 }
