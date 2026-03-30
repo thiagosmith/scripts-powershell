0f0044818fd6281f43b36de02a5a348b0c4420ab
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2026-03-29 23:02:44 -0300

    Update script06.ps1

--- script06.ps1
+++ script06.ps1
@@ -5,6 +5,6 @@ if (!$ip){
     echo "5m1th - OffSec Team"
     echo "Modo de uso: .\script.ps1 192.168.0.1"
 } else {
-echo "Detectando o alvo com endereço de IP: $ip"
+echo "Detectando o alvo com IP: $ip"
 ping -n 1 $ip | Select-String "bytes=32"
 }
