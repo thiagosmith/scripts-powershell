d745b8ed62847e171042a35aff04ececa0a42b1a
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2026-03-29 23:01:02 -0300

    Update script04.ps1

--- script04.ps1
+++ script04.ps1
@@ -1,6 +1,6 @@
 # Scripting Powershell
 echo "RedScan Academy - Curso Red Team"
 echo "5m1th - OffSec Team"
-$ip = Read-host "Informe o endereço de IP do alvo"
+$ip = Read-host "Informe o IP do target"
 $port = Read-host "Escolha a porta"
 echo "Atacando o alvo: $ip na porta: $port"
