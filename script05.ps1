dddc2aa1eb0c7d430b9a11995534ef9c6dd20921
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2026-03-29 23:01:42 -0300

    Update script05.ps1

--- script05.ps1
+++ script05.ps1
@@ -1,6 +1,6 @@
 # Scripting Powershell
 echo "RedScan Academy - Curso Red Team"
 echo "5m1th - OffSec Team"
-$ip = Read-Host "Informe o endereço de IP a ser detectado"
-echo "Detectando o alvo com endereço de IP: $ip"
+$ip = Read-Host "Informe o IP a ser detectado"
+echo "Detectando o alvo com IP: $ip"
 ping -n 1 $ip | Select-String "bytes=32"
