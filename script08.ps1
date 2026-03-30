fa48dbdd2cf8ec9475e08ae6247f3c12189ec258
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2026-03-29 22:47:40 -0300

    Update script08.ps1

--- script08.ps1
+++ script08.ps1
@@ -1,9 +1,9 @@
 # Scripting Powershell
 param($p1)
 if(!$p1){
-    echo "Mentoria OffSec - Scripting PowerShell"
-    echo "5m1th Security - OffSec Team"
-    echo "exemplo de uso: .\script.ps1 192.168.0"
+    echo "RedScan Academy - Curso Red Team"
+    echo "5m1th - OffSec Team"
+    echo "Modo de uso: .\script.ps1 192.168.0"
 } else {
 foreach ($ip in 1..254) {
 echo "Efetuando ping no host: $p1.$ip"
